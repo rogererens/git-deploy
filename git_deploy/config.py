efba5b134b49286ddc64a101ae850bf5b5c65f4a
rfaulk <rfaulk@yahoo-inc.com>
2013-12-26 14:38:03 -0800

    fix - custom syncing behaviour.

--- git_deploy/config.py
+++ git_deploy/config.py
@@ -45,6 +45,7 @@ exit_codes = {
     14: 'Commit failed, does not match HEAD. Exiting.',
     15: 'Missing git config. Aborting.',
     16: 'SSH call failed. Aborting.',
+    17: 'Call to deploy hook failed. Aborting.',
     19: 'Missing system configuration item "deploy.client-path". Exiting.',
     20: 'Cannot find top level directory for the git repository. Exiting.',
     21: 'Missing system configuration item "hook-dir". Exiting.',
--- git_deploy/drivers/driver.py
+++ git_deploy/drivers/driver.py
@@ -8,7 +8,6 @@ __license__ = 'GPL v2.0 (or later)'
 import subprocess
 import os
 
-from git_deploy.utils import ssh_command_target
 from git_deploy.git_methods import GitMethods
 from git_deploy.config import log, exit_codes, \
     DEFAULT_HOOK
@@ -91,33 +90,59 @@ class DeployDriverCustom(object):
                                                                     **kwargs)
         return cls.__instance
 
+    def _call_hooks(self, path, phase):
+        """Performs calls on path/phase dependent hooks
+
+        :param path: hooks path
+        :param phase: deploy phase
+
+        """
+        if os.path.exists(path):
+            sorted_path = sorted(os.listdir(path))
+            for item in sorted_path:
+                item_phase = item.split('.')[0]
+
+                # CALL hook and log
+                if phase == item_phase:
+                    cmd = path + '/' + item
+                    log_msg = 'CALLING \'{0}\' ON PHASE \'{1}\''.format(
+                      cmd, phase
+                    )
+                    log.info(__name__ + ' :: {0}'.format(log_msg))
+                    proc = subprocess.Popen(path + '/' + item,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+                    log.info(cmd + ' OUT -> ' + '; '.join(
+                        filter(lambda x: x, proc.communicate())))
+
+                    # Flag a failed hook
+                    if proc.returncode:
+                        raise DeployDriverError(exit_code=17, message=exit_codes[17])
+
+        else:
+            log.error(__name__ + ' :: CANNOT FIND HOOK PATH \'{0}\''.format(
+                path))
+            raise DeployDriverError(exit_code=17, message=exit_codes[17])
+
     def sync(self, args):
+        """Call custom syncing behaviour.
 
-        hook_path = "{0}/{1}".format(args['hook_dir'],
-                                     args['hook_script'])
+        1. Call common pre-sync hooks
+        2. Call app specific pre-sync hooks
+        3. Call app sync
+        4. Call app specific post-sync hooks
+        5. Call common post-sync hooks
 
-        # Call the sync script
-        if os.path.exists(hook_path):
-            log.info("{0} :: Calling sync "
-                     "script at {1}".format(__name__,
-                                            hook_path))
+        """
 
-            sync_cmd = "{0} --repo {1} --tag {2}".format(
-                hook_path,
-                args['repo_name'],
-                args['tag']
-            )
-            if args['force']:
-                sync_cmd = sync_cmd + ' --force'
-            proc = subprocess.Popen(sync_cmd.split())
-            proc_out = proc.communicate()[0]
+        # CALL deploy/apps/common
+        self._call_hooks(args['deploy_apps_common'], 'pre-sync')
 
-            if proc.returncode != 0:
-                exit_code = 40
-                log.error("{0} :: {1}".format(__name__,
-                                              exit_codes[exit_code]))
-                return exit_code
+        # CALL deploy/apps/$env
+        app_path = '{0}/{1}'.format(args['deploy_apps'], args.env)
+        self._call_hooks(app_path , 'pre-sync')
 
-            log.info("{0} :: SYNC SCRIPT OUT-> {1}".format(
-                __name__,
-                proc_out))
+        # CALL sync, deploy/apps/sync/$env.sync
+        self._call_hooks(app_path , args.env)
+        self._call_hooks(app_path , 'post-sync')
+        self._call_hooks(args['deploy_apps_common'] , 'post-sync')
