f152ed4eb4ffa959caa443470d24b6998d480c7f
rfaulk <rfaulk@yahoo-inc.com>
2014-03-19 23:53:59 -0700

    mod - update push/pull in git methods.

--- git_deploy/git_methods.py
+++ git_deploy/git_methods.py
@@ -5,6 +5,7 @@ Git methods
 __date__ = '2013-12-13'
 __license__ = 'GPL v2.0 (or later)'
 
+import sys
 import os
 import subprocess
 from datetime import datetime
@@ -20,6 +21,7 @@ from dulwich.repo import Repo
 from dulwich.objects import Tag, Commit, parse_timezone
 from dulwich.diff_tree import tree_changes
 from dulwich.client import get_transport_and_path
+from dulwich.errors import SendPackError, UpdateRefsError
 
 from config import log, exit_codes, configure
 
@@ -240,37 +242,60 @@ class GitMethods(object):
                                           key=lambda t: (t[1].commit_time, t)))
         return ordered_tags
 
-    def _dulwich_push(self, git_url, branch):
-        """
-        Remote push with dulwich via dulwich.client
+    def _dulwich_push(self, remote_location, refs_path,
+                      outstream=sys.stdout, errstream=sys.stderr):
+        """Remote push with dulwich via dulwich.client
+
+        :param repo : Path to repository
+        :param remote_location: Location of the remote
+        :param refs_path: relative path to the refs to push to remote
+        :param outstream: A stream file to write output
+        :param errstream: A stream file to write errors
         """
 
+
         # Open the repo
-        _repo = Repo(self.config['top_dir'])
+        r = Repo(self.config['top_dir'])
 
         # Get the client and path
-        client, path = get_transport_and_path(git_url)
+        client, path = get_transport_and_path(remote_location)
 
         def update_refs(refs):
-            new_refs = _repo.get_refs()
-            new_refs['refs/remotes/origin/' + branch] = new_refs['HEAD']
+            new_refs = r.get_refs()
+            refs[refs_path] = new_refs['HEAD']
             del new_refs['HEAD']
-            return new_refs
+            return refs
 
-        client.send_pack(path, update_refs,
-                         _repo.object_store.generate_pack_contents)
+        try:
+            client.send_pack(path, update_refs,
+                r.object_store.generate_pack_contents, progress=errstream.write)
+            outstream.write("Push to %s successful.\n" % remote_location)
+        except (UpdateRefsError, SendPackError) as e:
+            outstream.write("Push to %s failed.\n" % remote_location)
+            errstream.write("Push to %s failed -> '%s'\n" % e.message)
 
-    def _dulwich_pull(self, git_url):
-        """ Pull from remote via dulwich.client """
+    def _dulwich_pull(self, remote_location, refs_path, errstream=sys.stderr):
+        """ Pull from remote via dulwich.client
+
+        :param repo: Path to repository
+        :param remote_location: Location of the remote
+        :param refs_path: relative path to the fetched refs
+        :param outstream: A stream file to write to output
+        :param errstream: A stream file to write to errors
+        """
+
 
         # Open the repo
-        _repo = Repo(self.config['top_dir'])
+        r = Repo(self.config['top_dir'])
 
-        client, path = get_transport_and_path(git_url)
-        remote_refs = client.fetch(path, _repo)
-        _repo['HEAD'] = remote_refs['refs/heads/master']
+        client, path = get_transport_and_path(remote_location)
+        remote_refs = client.fetch(path, r, progress=errstream.write)
+        r['HEAD'] = remote_refs[refs_path]
 
-        self._dulwich_checkout(_repo)
+        # Perform 'git checkout .' - syncs staged changes
+        indexfile = r.index_path()
+        tree = r["HEAD"].tree
+        index.build_index_from_tree(r.path, indexfile, r.object_store, tree)
 
     def _dulwich_checkout(self, _repo):
         """ Perform 'git checkout .' - syncs staged changes """
